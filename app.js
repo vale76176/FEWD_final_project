b6d6a8c705da951d10ce36ea5f5f1222821cc8ac
vale76176 <dulcemacarrillomi@gmail.com>
2020-08-31 22:32:52 -0700

    WIP
    Still have CSS work to do. Will focus on attaching the form and getting fuctionality next session.

--- app.js
+++ app.js
@@ -1,12 +1,51 @@
-const scriptURL = 'source google page form'
-const form = document.querySelector('form')
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-        .then(response => console.log('Success!', response))
-        .then(() => {
-            //This is where you write your code
-            //Do not alter below this line
-        })
-        .catch(error => console.error('Error!', error.message))
+console.log('hello')
+
+let currentImgIndex = 0
+
+const images = ['images/bernd.png', 'images/dulce.png', 'images/marloes.png', 'images/maxime.png', 'images/nadja.png',  'images/paul.png']
+
+const mainImage = document.getElementById('mainImage')
+
+console.log('mainImage')
+
+mainImage.setAttribute('src', images[currentImgIndex])
+
+const next = document.querySelector('.next')
+const prev = document.querySelector('.prev')
+
+console.log('next')
+console.log('prev')
+
+next.addEventListener('click', () => {
+    if(currentImgIndex >= images.length - 1){
+        currentImgIndex = 0
+    } else {
+        currentImgIndex += 1
+    }
+    mainImage.setAttribute('src', images[currentImgIndex])
+    
 })
+
+prev.addEventListener('click', () => {
+    if(currentImgIndex >= images.length - 1){
+        currentImgIndex = 0
+    } else {
+        currentImgIndex += 1
+    }
+    mainImage.setAttribute('src', images[currentImgIndex])
+  })
+
+
+
+
+
+// const scriptURL = 'source google page form'
+// const form = document.querySelector('form')
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault()
+//     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//         .then(response => console.log('Success!', response))
+//         .then(() => {
+//           })
+//         .catch(error => console.error('Error!', error.message))
+// })
