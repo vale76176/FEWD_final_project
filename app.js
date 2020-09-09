b61d783f3fceb9d5417b35f8da401137c8ee816b
vale76176 <dulcemacarrillomi@gmail.com>
2020-09-08 21:28:10 -0700

    Carousel cleanup + Validated form
    Fixed carousel and validated form. Published app form. Need to clean up rows "name" in input tags and figure out how to deal with multiple choice.

--- app.js
+++ app.js
@@ -34,7 +34,44 @@ prev.addEventListener('click', () => {
     }
     mainImage.setAttribute('src', images[currentImgIndex])
   })
-  
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxZp258TUvhXocnq57q7jhvC-Tt-ayWMwrX-xwmzl_WcvVIl1U/exec'
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => console.log('Success!', response))
+        .then(() => {
+            //This is where you write your code
+
+
+
+
+            //Do not alter below this line
+        })
+        .catch(error => console.error('Error!', error.message))
+})
+
+// const form = document.getElementById('application-form')
+// // console.log(form)
+// const state = document.getElementById('state')
+// // console.log(state)
+// const error = document.querySelector('#error')
+// // console.log(error)
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault()
+//     // console.log('this may work')
+//     if(state.value !== 'NJ'){
+//         error.innerText = 'Error: Applicant must reside in NJ'
+//         error.style.color = 'red'
+//         // alert('Hey we don\'t use alerts anymore')
+//     } else {
+//         error.innerText = ""
+//     }
+// })
+// -------- html
+
 
 
 // const scriptURL = 'source google page form'
